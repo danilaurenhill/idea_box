1c41de115083d1a6f78b2bf7ce362d373b9d4e19
Danielle Hill <starson3@aol.com>
2013-07-12 13:46:31 -0400

    initial commit

---
+++ app.rb
@@ -0,0 +1,20 @@
+require './idea'
+class IdeaBoxApp < Sinatra::Base
+  configure :development do
+  	register Sinatra::Reloader
+  end
+
+  get '/' do
+    erb :index, locals: {ideas: Idea.all}
+  end
+  
+  not_found do
+  	erb :error
+  end
+ 
+  post '/' do
+  	idea = Idea.new(params['idea_title'], params['idea_description'])
+    idea.save
+    redirect '/'
+  end
+end
